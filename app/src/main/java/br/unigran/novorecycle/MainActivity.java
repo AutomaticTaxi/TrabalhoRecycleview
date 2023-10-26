22f96a50ad513f732ab6505979a12fc514b88e1c
Lucas <lucas.antunes.santos.81@gmail.com>
2023-10-25 21:11:56 -0400

    Atulizado Ler atualizando a recycleview

--- app/src/main/java/br/unigran/novorecycle/MainActivity.java
+++ app/src/main/java/br/unigran/novorecycle/MainActivity.java
@@ -114,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
 
                         pessoas.add(dados.getValue(Pessoa.class));
                     }
-
+                    recyclerView.getAdapter().notifyDataSetChanged();
                 }
 
             }
